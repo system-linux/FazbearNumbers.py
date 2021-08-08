c2ea739fe5cd45f5651146d1389034c5588eac26
FazbearEntertainment <84124265+system-linux@users.noreply.github.com>
2021-08-08 23:45:11 +0300

    Number

--- FazbearNumbers.py
+++ FazbearNumbers.py
@@ -1,9 +1,11 @@
 import time
 import random
+"""
 import colorama
 from colorama import Fore, Back, Style
 colorama.init()
 print(Fore.MAGENTA)
+"""
 time.sleep(1.4)
 print("""                ----------|
 	        |. -------|
@@ -14,7 +16,9 @@ print("""                ----------|
 	        |.|
 	        |-|""")
 time.sleep(0.8)
+"""
 print(Fore.YELLOW)
+"""
 #sekilli yazmak zor
 print("""
 	         ==============\                       
@@ -29,7 +33,9 @@ print("""
 	        |.     -------         |
 	        |=====================""")
 time.sleep(1)
+"""
 print(Fore.BLUE)
+"""
 print("""
          -------------
          |.    ----  |
@@ -44,7 +50,9 @@ print("""
          |__|     |_|
 """)
 time.sleep(1)
+"""
 print(Fore.MAGENTA + Back.BLUE)
+"""
 print("FREDDY FAZBEAR EĞLENCEYE HOŞGELDİNİZ!")
 time.sleep(2.5)
 time.sleep(1)
@@ -52,13 +60,19 @@ time.sleep(0.9)
 tutulan = random.randint(1 , 10)
 söylenen = input('sence hangi sayıyı tuttum :')
 if(söylenen==tutulan):
+    """
     print(Fore.YELLOW + Back.GREEN)
+    """
     print('Great job!👌!')
     
 else:
+    """
     print(Fore.YELLOW + Back.RED)
+    """
     print('Bad👎')
+    """
     print(Fore.MAGENTA + Back.BLUE)
+    """
     print('Tutuğum sayı')
     time.sleep(1)
     print(tutulan)
